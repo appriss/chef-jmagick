f0c79194883a5b60a54dbce7dee40523206ee2f9
Donavan Pantke <dpantke@appriss.com>
2014-07-28 09:41:06 -0400

    Retry test

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,7 @@
 include_recipe "imagemagick::devel"
 include_recipe "build-essential"
 
-#include_recipe "java"
+include_recipe "java"
 
 execute "Test Java Version" do
 	command "java -version >/tmp/version.out"
