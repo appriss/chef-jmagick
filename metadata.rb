4bd20453384536eae2acd8d8f865bf495c85038d
Donavan Pantke <dpantke@appriss.com>
2014-07-28 10:15:14 -0400

    Switch to default recipe.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures jmagick'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-recipe "jmagick", "Installs the JMagick library."
+recipe "jmagick::default", "Installs the JMagick library."
 
 
 depends "java"
